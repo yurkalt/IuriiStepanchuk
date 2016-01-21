bac9336df860720470a55b566aaad64f946937fb
Iurii <stepanchuck@gmail.com>
2016-01-21 13:14:35 +0200

    + Console

--- src/Week1/TestMyConsole.java
+++ src/Week1/TestMyConsole.java
@@ -12,26 +12,37 @@ public class TestMyConsole {
 
         Scanner scan = new Scanner(System.in);
 
-
-
-
-
-
         System.out.println("Введіть перше число:");
         int numScan1 = scan.nextInt();
         MyConsole num1 = new MyConsole(numScan1);
+
+        System.out.println("Введіть операцію (+,-,*,/):");
+        String operScan = scan.next();
 
         System.out.println("Введіть друге число:");
         int numScan2 = scan.nextInt();
         MyConsole num2 = new MyConsole(numScan2);
 
-        //Sum
-        MyConsole num3 = num1.add(num2);
-        System.out.println(num1.getValue() + " + " + num2.getValue() + " = " + num3.getValue());
-
-        //Minus
-        MyConsole num4 = num1.rob(num2);
-        System.out.println(num1.getValue() + " - " + num2.getValue() + " = " + num4.getValue());
+        switch (operScan) {
+            case "+":  MyConsole num3 = num1.add(num2);
+                System.out.println("Результат:");
+                System.out.println(num1.getValue() + " + " + num2.getValue() + " = " + num3.getValue());
+                break;
+            case "-":
+                System.out.println("Результат:");
+                MyConsole num4 = num1.rob(num2);
+                System.out.println(num1.getValue() + " - " + num2.getValue() + " = " + num4.getValue());
+                break;
+            case "*":
+                System.out.println("Результат:");
+                MyConsole num5 = num1.mult(num2);
+                System.out.println(num1.getValue() + " * " + num2.getValue() + " = " + num5.getValue());
+                break;
+            case "/":
+                System.out.println("Результат:");
+                System.out.println(num1.getValue() + " / " + num2.getValue() + " = " + num1.devide(num2));
+                break;
+        }
 
     }
 }
