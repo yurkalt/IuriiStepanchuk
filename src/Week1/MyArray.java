c9fb5be7ad8e7181e738f5365b8393f14882e4e9
Iurii <stepanchuck@gmail.com>
2016-01-22 10:13:18 +0200

    + Array

--- src/Week1/MyArray.java
+++ src/Week1/MyArray.java
@@ -31,12 +31,60 @@ public class MyArray {
         return max;
     }
 
-    public int [] random (int length) {
-        int [] random = new int [length];
-        for (int i = 0; i < length ; i++) {
-            random [i] = (int)(Math.random()*100);
+    public MyArray random () {
+        for (int i = 0; i < this.array.length ; i++) {
+            this.array [i] = (int)(Math.random()*100);
         }
-        return random;
+        return this;
+    }
+
+    public MyArray enlarge (int add) {
+        MyArray newArray = new MyArray(new int [this.array.length + add]);
+
+        for (int i = 0; i < this.array.length; i++) {
+            newArray.array[i] = this.array[i];
+        }
+        return newArray;
+    }
+
+
+
+    public MyArray sort(){
+        MyArray sorted = new MyArray(new int [this.array.length]);
+        //Copy from this to sort:
+        for (int i = 0; i < this.array.length; i++) {
+            sorted.array[i] = this.array[i];
+        }
+        //Sort sorted array:
+        for (int i = sorted.array.length; i>=0 ; i--) {
+            for (int j = 0; j <sorted.array.length-1 ; j++) {
+                int k = j+1;
+                if (sorted.array[j] > sorted.array[k]) {
+                    swapnumbers(j,k,sorted.array);
+                }
+            }
+        }
+        return sorted;
+    }
+
+    public static void swapnumbers (int i, int j, int [] mas) {
+        int temp = mas[i];
+        mas[i] = mas[j];
+        mas[j]=temp;
+    }
+
+    public String compare (MyArray other) {
+        if (this.array.length == other.array.length) {
+            for (int i = 0; i < other.array.length; i++) {
+                if (this.array[i] != other.array[i]) {
+                  return "Arrays have same length but elements are not the same!";
+                }
+            }
+            return "Arrays are the same!!!";
+        }
+        else {
+            return "Array are not the same!";
+        }
     }
 
     public void print () {
--- src/Week1/TestMyArray.java
+++ src/Week1/TestMyArray.java
@@ -8,21 +8,43 @@ public class TestMyArray {
     public static void main(String[] args) {
 
         MyArray array = new MyArray( new int[] {4,5,232,3,-1});
-// Minimal
 
+        // Minimal
         System.out.print("Smallest element of array ");
         array.print();
         System.out.println(array.min());
-//Maximum
 
-
+        //Maximum
         System.out.print("Biggest element of array ");
         array.print();
         System.out.println(array.max());
-        MyArray array1 = new MyArray(new int [7]);
+
+        // Random
+        System.out.println("Random array :");
+        MyArray randomArray = new MyArray(new int [5]);
+        randomArray.random();
+        randomArray.print();
+
+        // Enlarge array
+        System.out.println("Array enlarged on 4 elements :");
+        MyArray enlarger = array.enlarge(4);
+        enlarger.print();
+
+        //Sort array:
+        System.out.println("Sorted array:");
+        MyArray sorted = array.sort();
+        sorted.print();
+
+        // Compare with random
+        System.out.println("Compare array:");
+        array.print();
+        System.out.println("and random array:");
+        randomArray.print();
+        System.out.println("Result:");
+        System.out.println(array.compare(randomArray));
+
+
+
+
     }
-
-
-
-
 }
